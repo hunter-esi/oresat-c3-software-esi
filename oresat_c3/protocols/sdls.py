95b7ae8aa2eaa38de72b5bb1192c1f3be0441551
Hunter-McClellan <huntermcclellan0@gmail.com>
2026-06-01 16:33:51 -0600

    Migrated SDLS to a class based approach.

--- oresat_c3/protocols/sdls.py
+++ oresat_c3/protocols/sdls.py
@@ -12,9 +12,6 @@ HMAC_LEN = 32
 SPI_LEN = 2
 SEQ_NUM_LEN = 4
 
-SPI_LIST = [1, 1, 0] # spi definition.
-# There should be some VC parameters class that COP-1 and SDLS reference when determining how to handle VCs.
-
 class SdlsInvalidHmacError(Exception):
     pass
 
@@ -23,81 +20,29 @@ def gen_hmac(hmac_key: bytes, message: bytes) -> bytes:
 
     return hmac.digest(hmac_key, message, hashlib.sha3_256)
 
-def get_sdls_len(vcid: int) -> int:
-    """Get the length of the header and trailer for the SPI associated with the given Virtual Channel.
 
-    Parameters
-    ----------
-    vcid
-        The Virtual Channel Identifier to retrieve the SPI from.
+class SdlsEmpty:
+    def len(self) -> int:
+       return 0
 
-    Returns
-    -------
-    int
-        The length of the sdls header and trailer.
-    """
-    
-    if len(SPI_LIST) <= vcid:
-        raise ValueError(f"VCID {vcid} does not have corresponding SPI.")
+    def header_len(self) -> int:
+        return 0
 
-    spi = SPI_LIST[vcid]
-    
-    if spi == 1:
-        return HMAC_LEN + SPI_LEN + SEQ_NUM_LEN
-    elif spi == 0:
-        return 0;
-    raise ValueError(f"VCID has invalid SPI: {spi}")
+    def apply(self, frame: TransferFrame, seq_num: int, hmac_key: bytes) -> None:
+        return
+
+    def verify(self, TransferFrame, hmac_key: bytes) -> int:
+        return 0
 
 
-def get_sdls_header_len(vcid: int) -> int:
-    """Get the length of the header for the SPI associated with the given Virtual Channel.
+class SdlsOresat(SdlsEmpty):
+    def len(self) -> int:
+       return HMAC_LEN + SPI_LEN + SEQ_NUM_LEN
 
-    Parameters
-    ----------
-    vcid
-        The Virtual Channel Identifier to retrieve the SPI from.
-
-    Returns
-    -------
-    int
-        The length of the sdls header.
-    """
-    
-    if len(SPI_LIST) <= vcid:
-        raise ValueError(f"VCID {vcid} does not have corresponding SPI.")
-
-    spi = SPI_LIST[vcid]
-    
-    if spi == 1:
+    def header_len(self) -> int:
         return SPI_LEN + SEQ_NUM_LEN
-    elif spi == 0:
-        return 0;
-    raise ValueError(f"VCID has invalid SPI: {spi}")
 
-
-def apply_sdls(frame: TransferFrame, seq_num: int, hmac_key: bytes) -> None:
-    """Apply the HMAC to the data zone (out of spec but no other choice) and return the header to be put in the insert zone (see previous note.)
-
-    Parameters
-    ----------
-    frame
-        Transfer frame to apply sdls to.
-    seq_num
-        Anti replay sequence number to apply in the sdls header.
-    hmac_key
-        The hmac key used to compute the authentication HMAC.
-
-    Returns
-    -------
-    None
-    """
-
-    if len(SPI_LIST) <= frame.header.vcid:
-        raise ValueError(f"VCID {frame.header.vcid} does not have corresponding SPI.")
-
-    spi = SPI_LIST[frame.header.vcid]
-    
-    if spi == 1:
+    def apply(self, frame: TransferFrame, seq_num: int, hmac_key: bytes) -> None:
         sdls_header = bytearray(b"\x00\x01") + seq_num.to_bytes(SEQ_NUM_LEN, "little")
 
         frame.insert_zone = sdls_header
@@ -116,36 +61,7 @@ def apply_sdls(frame: TransferFrame, seq_num: int, hmac_key: bytes) -> None:
         frame.tfdf.tfdz = frame.tfdf.tfdz + hmac_val
         return
 
-    elif spi == 0:
-        return
-
-    raise ValueError(f"VCID has invalid SPI: {spi}")
-
-
-def verify_sdls(frame: TransferFrame, hmac_key: bytes) -> int:
-    """Check that the HMAC is good, then return the sequence number. Sequence number handling should be handled here, but I do not
-    have time to disentangle it now. TODO: fix.
-
-    Parameters
-    ----------
-    frame
-        The transfer frame as a TransferFrame object
-    hmac_key
-        The contents of the transfer frame. As the spacepackets protocol does not support sdls, the MAC will be inserted into the 
-        data zone, despite spec stating that this is not the case.
-
-    Returns
-    -------
-    int
-        The sequence number that is handled not here.
-    """
-
-    if len(SPI_LIST) <= frame.header.vcid:
-        raise ValueError(f"VCID {frame.header.vcid} does not have corresponding SPI.")
-
-    spi = SPI_LIST[frame.header.vcid]
-    
-    if spi == 1:
+    def verify(self, TransferFrame, hmac_key: bytes) -> int:
         sdls_header = frame.insert_zone
         payload = frame.tfdf.pack()[:-HMAC_LEN]
         authenticated_data = frame.header.pack() + sdls_header + payload
@@ -167,7 +83,93 @@ def verify_sdls(frame: TransferFrame, hmac_key: bytes) -> int:
         sequence_number = int.from_bytes(sdls_header[:-SEQ_NUM_LEN], byteorder="little")
         return sequence_number
 
-    elif spi == 0:
-        return 0
 
-    raise ValueError(f"VCID has invalid SPI: {spi}")
+# There should be some VC parameters class that sets how VCs interact with COP-1 and SDLS.
+SPI_LIST = [SdlsOresat(), SdlsOresat(), SdlsEmpty()]  # spi definition.
+
+def get_sdls_len(vcid: int) -> int:
+    """
+    Get the length of the header and trailer for the SPI associated with the given Virtual Channel.
+
+    Parameters
+    ----------
+    vcid
+        The Virtual Channel Identifier to retrieve the SPI from.
+
+    Returns
+    -------
+    int
+        The length of the sdls header and trailer.
+    """
+    
+    try:
+        return SPI_LIST[vcid].len()
+    except IndexError as e:
+        raise ValueError(f"VCID {vcid} does not have corresponding SPI.") from e
+
+def get_sdls_header_len(vcid: int) -> int:
+    """
+    Get the length of the header for the SPI associated with the given Virtual Channel.
+
+    Parameters
+    ----------
+    vcid
+        The Virtual Channel Identifier to retrieve the SPI from.
+
+    Returns
+    -------
+    int
+        The length of the sdls header.
+    """
+
+    try:
+        return SPI_LIST[vcid].header_len()
+    except IndexError as e:
+        raise ValueError(f"VCID {vcid} does not have corresponding SPI.") from e
+
+def apply_sdls(frame: TransferFrame, seq_num: int, hmac_key: bytes) -> None:
+    """
+    Apply the HMAC to the data zone (out of spec but no other choice) and return the header to be put in the insert zone (see previous note.)
+
+    Parameters
+    ----------
+    frame
+        Transfer frame to apply sdls to.
+    seq_num
+        Anti replay sequence number to apply in the sdls header.
+    hmac_key
+        The hmac key used to compute the authentication HMAC.
+
+    Returns
+    -------
+    None
+    """
+
+    try:
+        return SPI_LIST[frame.header.vcid].apply()
+    except IndexError as e:
+        raise ValueError(f"VCID {vcid} does not have corresponding SPI.") from e
+
+def verify_sdls(frame: TransferFrame, hmac_key: bytes) -> int:
+    """
+    Check that the HMAC is good, then return the sequence number. Sequence number handling should be handled here, but I do not
+    have time to disentangle it now. TODO: fix.
+
+    Parameters
+    ----------
+    frame
+        The transfer frame as a TransferFrame object
+    hmac_key
+        The contents of the transfer frame. As the spacepackets protocol does not support sdls, the MAC will be inserted into the 
+        data zone, despite spec stating that this is not the case.
+
+    Returns
+    -------
+    int
+        The sequence number that is handled not here.
+    """
+
+    try:
+        return SPI_LIST[frame.header.vcid].verify()
+    except IndexError as e:
+        raise ValueError(f"VCID {vcid} does not have corresponding SPI.") from e
