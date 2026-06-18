2e2527a5efa7f9ff3350318b6bff7c02abbc1d47
Hunter-McClellan <huntermcclellan0@gmail.com>
2026-06-18 10:43:38 -0600

    Created basic payload service. Realized that I should rework cfdp first.

---
+++ oresat_c3/services/payload.py
@@ -0,0 +1,18 @@
+"""
+Payload Service:
+Manage the operation of a given payload, Will be designed for OSIRIS first, but with the intention
+of extending it to other payloads later.
+"""
+
+from os.path import abspath
+from time import monotonic
+
+from olaf import Service
+
+class PayloadService(Service):
+    def __init__():
+        self._state = None
+
+
+
+
