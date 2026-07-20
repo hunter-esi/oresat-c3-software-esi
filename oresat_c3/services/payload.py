d4113b9b5dda3eac378e73625e2d85e103e6ee7c
Hunter-McClellan <huntermcclellan0@gmail.com>
2026-07-20 15:23:50 -0600

    beecon payload no longer uses nodemanager, but handles the node itself. given that the processor for the science card will be unused, this is the best way to handle the beecon.

--- oresat_c3/services/payload.py
+++ oresat_c3/services/payload.py
@@ -11,6 +11,7 @@ from time import monotonic, time  # noqa: F401
 from canopen.objectdictionary import ODVariable
 from olaf import Service, logger
 
+from ..subsystems.opd import OpdNode
 from .node_manager import NodeManagerService
 
 
@@ -48,41 +49,22 @@ class BeeconHandler():
     # Beecon state pseudoenum:
     # 0: off
     # 1: on
+    _I2C_BUS_NUM = 2
 
-    def __init__(self, in_state: ODVariable, node_mgr: NodeManagerService) -> None:
+    def __init__(self, in_state: ODVariable) -> None:
         self._state = in_state
-        self._node_mgr = node_mgr
+        self._beecon_node = OpdNode(self._I2C_BUS_NUM, "beecon", 0x10)
 
     def loop(self) -> None:
         """Runs the beecon state machine. Makes sure the beecon is on or off, depending on state"""
-        nmgr_beecon = self._node_mgr.node_status("beecon_sci")
-
-        if nmgr_beecon == 0xFF:
-            logger.error("OPD says beecon is dead!")
-            time.sleep(10)
-            return
-        elif nmgr_beecon == 4:
-            logger.error("Could not find beecon science card through OPD!")
-            time.sleep(10)
-            return
-
         state_val = self._state.value
         if state_val == 0:
             # check if the beecon is off. If not, turn it off.
-            if nmgr_beecon != 0:
-                self._node_mgr.disable("beecon_sci")
+            if self._beecon_node.is_enabled:
+                self._beecon_node.disable()
         elif state_val == 1:
-            # check if the beecon is off. If not, turn it off.
-            if nmgr_beecon == 2:
-                pass
-            elif nmgr_beecon == 0:
-                self._node_mgr.enable("beecon_sci")
-            elif nmgr_beecon == 1:
-                logger.debug("beecon science card is booting")
-            elif nmgr_beecon == 3:
-                logger.debug("beecon science card is booting")
-            elif nmgr_beecon in (5, 6):
-                logger.debug(f"beecon science card is in incoherent state! {nmgr_beecon}")
+            if not self._beecon_node.is_enabled:
+                self._beecon_node.enable()
         else:
             logger.error("beecon service got incoherent state")
         time.sleep(1)
