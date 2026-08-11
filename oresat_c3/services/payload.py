c9e42806be9ac915d54460cfe84272b2c53b9532
Hunter-McClellan <huntermcclellan0@gmail.com>
2026-08-11 13:38:06 -0600

    Fixed battery issue

--- oresat_c3/services/payload.py
+++ oresat_c3/services/payload.py
@@ -37,6 +37,10 @@ class PayloadService(Service):
         self._state = self.node.od["payload_ctrl"]["state"]
         self._enabled = self.node.od["payload_ctrl"]["enabled"]
 
+        bat_1_rec = self.node.od["battery_1"]
+        self._vbatt_bp1_obj = bat_1_rec["pack_1_vbatt"]
+        self._vbatt_bp2_obj = bat_1_rec["pack_2_vbatt"]
+
         if self._mission.__str__() == "osiris_b1":
             logger.info("creating osiris payload handler")
             # self._payload_handler = BeeconHandler(self._state)
